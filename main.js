a390df5970dec6349007ab9517e98dd7bf4fe984
AzTecaher <az.teachers.2020@gmail.com>
2022-02-03 00:02:58 +0300

    Frontend Giris

---
+++ main.js
@@ -0,0 +1,11 @@
+var buton1 = document.getElementById("btn1");
+buton1.addEventListener("click",function(){
+alert("Kanalımıza Abunə ol. Birlikdə Böyüyək");
+})
+
+var buton2 = document.getElementById("btn2");
+buton2.addEventListener("click",function(){
+var txt = document.querySelector(".text");
+txt.style.color = "red";
+txt.style.backgroundColor = "yellow";
+})
